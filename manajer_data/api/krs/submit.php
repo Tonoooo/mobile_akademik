6f7ec1c6e4a4f87d14a5ca8555a17ad7e91e0da7
tonoooo <adomtonot@gmail.com>
2026-01-12 18:00:27 +0700

    izin

--- manajer_data/api/krs/submit.php
+++ manajer_data/api/krs/submit.php
@@ -9,7 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-
+// izin
 include_once '../../config/database.php';
 
 // $conn is defined in database.php
